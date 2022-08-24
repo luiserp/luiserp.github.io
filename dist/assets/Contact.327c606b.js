12b9ee7f3c66a33feb95f4cd52a2ac62dac66fd3
luiserp <luisreyesperez98@gmail.com>
2022-08-24 12:54:04 +0200

    gh-pages

---
+++ dist/assets/Contact.327c606b.js
@@ -0,0 +1,1 @@
+import{_ as e,a as t,c,b as o}from"./index.503a3729.js";const n={},s=o("h1",null,"Contact",-1),a=[s];function _(r,l){return t(),c("div",null,a)}const f=e(n,[["render",_]]);export{f as default};
